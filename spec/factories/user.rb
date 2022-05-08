269d2fbd7271f942000bdcdb2c93247c0bb6c603
imomochiii <orange.piano88@gmail.com>
2022-05-09 05:41:02 +0900

    テストコード記述

---
+++ spec/factories/user.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             { 'すずき' }
+    first_name            { 'たろう' }
+    last_name_kana        { 'ズズキ' }
+    first_name_kana       { 'タロウ' }
+    birthday              {Faker::Date.birthday}
+  end
+end
