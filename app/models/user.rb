c9d75cdb75d0f01088f1972c69313a3172b93965
imomochiii <orange.piano88@gmail.com>
2022-05-09 03:25:11 +0900

    バリデーションの訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+  validates :nickname, presence: true
+  validates :birthday, presence: true
   with_options presence: true do
-    # ひらがな、カタカナ、漢字のみ許可する
     validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
-
-    # カタカナのみ許可する
     validates :last_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
   end
