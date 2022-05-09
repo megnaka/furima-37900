1360d1f9450c69431f61221ca069d8821dd903f2
imomochiii <orange.piano88@gmail.com>
2022-05-09 16:12:31 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :delivery_payer, :prefecture, :sending_days
+
+  with_options presence: true do
+    validates :item_name, :description
+    validates :price,  numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+    with_options numericality: { other_than: 0 , message: "can't be blank"} do
+      validates :category, :condition, :delivery_payer, :prefecture, :sending_days
+    end
+  end
 end
