8067068795989fd7e2633957b09addd8daf90200
imomochiii <orange.piano88@gmail.com>
2022-05-09 12:45:39 +0900

    バリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: ' Include both letters and numbers' }
-  validates :nickname, presence: true
-  validates :birthday, presence: true
   with_options presence: true do
+    validates :nickname
+    validates :birthday
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
