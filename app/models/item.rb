f299d60eacb6d9357a993056f095c818f2beb85f
imomochiii <orange.piano88@gmail.com>
2022-05-10 17:35:35 +0900

    priceのバリデーションを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,9 +13,10 @@ class Item < ApplicationRecord
     validates :image
     validates :item_name
     validates :description
-    validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid.Input half-width characters' }, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+    validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters" }
   end
 
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
   validates :category_id, numericality: { other_than: 0 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 0 , message: "can't be blank"}
   validates :delivery_payer_id, numericality: { other_than: 0 , message: "can't be blank"}
