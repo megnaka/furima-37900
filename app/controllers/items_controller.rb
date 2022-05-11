4dbd0b05f54b847f53f5b4467101352fb3b04686
imomochiii <orange.piano88@gmail.com>
2022-05-11 20:32:05 +0900

    ページ遷移の制限を設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -42,5 +43,10 @@ class ItemsController < ApplicationController
   def items_params
     params.require(:item).permit(:item_name, :image, :description, :category_id, :condition_id, :delivery_payer_id,
                                  :prefecture_id, :sending_days_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
   end
 end
