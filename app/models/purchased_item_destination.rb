85874f363edd20502e96e9866d8e40deeb681383
imomochiii <orange.piano88@gmail.com>
2022-05-12 23:24:30 +0900

    テストコードを実装

--- app/models/purchased_item_destination.rb
+++ app/models/purchased_item_destination.rb
@@ -5,15 +5,12 @@ class PurchasedItemDestination
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :city
-  end
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-  with_options presence: true do
     validates :building_address
-    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is too short" }
+    validates :phone_number, format: {with: /\A\d{10,11}\z/}
     validates :user_id
     validates :item_id
   end
-  validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number" }
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
     purchased_item = PurchasedItem.create(user_id: user_id, item_id: item_id)
