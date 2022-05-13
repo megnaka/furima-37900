3a2b2e032b49b4bdc288722854f4315e4a1b17cd
imomochiii <orange.piano88@gmail.com>
2022-05-13 19:59:25 +0900

    Revert "購入情報を保存"
    This reverts commit 7424e954628970a2ec93c74ab0250a905cf16ab0.

--- app/controllers/purchased_items_controller.rb
+++ app/controllers/purchased_items_controller.rb
@@ -6,19 +6,6 @@ class PurchasedItemsController < ApplicationController
   end
   
   def create
-    @item = Item.find(params[:item_id])
-    @purchased_item_destination = PurchasedItemDestination.new(purchased_item_params)
-    if @purchased_item_destination.valid?
-      @purchased_item_destination.save
-      redirect_to root_path
-    else
-      render :index
-    end
-  end
-
-  private
-
-  def purchased_item_params
-    params.require(:purchased_item_destination).permit(:postal_code, :prefecture_id, :city, :building_address, :building_name, :phone_number).merge(user_id: current_user.id)
+    binding.pry
   end
 end
