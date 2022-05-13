41fe41256582f33ffd25f2e5ea5385230dad8983
imomochiii <orange.piano88@gmail.com>
2022-05-13 20:03:36 +0900

    Revert "Revert "決済機能を実装""
    This reverts commit 3972239e282f847562f584c2e8ba4ec487a0dd4c.

--- app/controllers/purchased_items_controller.rb
+++ app/controllers/purchased_items_controller.rb
@@ -9,6 +9,7 @@ class PurchasedItemsController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchased_item_destination = PurchasedItemDestination.new(purchased_item_params)
     if @purchased_item_destination.valid?
+      pay_item
       @purchased_item_destination.save
       redirect_to root_path
     else
@@ -16,9 +17,19 @@ class PurchasedItemsController < ApplicationController
     end
   end
 
+
   private
 
   def purchased_item_params
-    params.require(:purchased_item_destination).permit(:postal_code, :prefecture_id, :city, :building_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchased_item_destination).permit(:postal_code, :prefecture_id, :city, :building_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_4a9edb4e313b027808ba9d8c"
+    Payjp::Charge.create(
+      amount: Item.find(params[:item_id])[:price],
+      card: purchased_item_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/purchased_item_destination.rb
+++ app/models/purchased_item_destination.rb
@@ -1,8 +1,9 @@
 class PurchasedItemDestination
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :building_address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :building_address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
+    validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :city
     validates :building_address
@@ -14,6 +15,6 @@ class PurchasedItemDestination
 
   def save
     purchased_item = PurchasedItem.create(user_id: user_id, item_id: item_id)
-    Destination.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, building_address: building_address, building_name: building_name, phone_number: phone_number, purchased_item_id: purchased_item_id)
+    Destination.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, building_address: building_address, building_name: building_name, phone_number: phone_number, purchased_item_id: purchased_item.id)
   end
 end
