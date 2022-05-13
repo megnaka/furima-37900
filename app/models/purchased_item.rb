7ab85d17d1af4704800ad632c9519d12e2fe3b9a
imomochiii <orange.piano88@gmail.com>
2022-05-13 20:00:05 +0900

    Revert "formオブジェクトを作成"
    This reverts commit 40a72f5eca1f00d4d7b44907e4b946020a96fd75.

--- app/models/purchased_item.rb
+++ app/models/purchased_item.rb
@@ -1,26 +1,5 @@
-class PurchasedItem
-  include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :building_address, :building_name, :phone_number, :purchased_item_id, :user_id, :item_id
-
-  with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :city
-    validates :building_address
-    validates :phone_number, format: {with: /\A\d{10,11}\z/ }
-    validates :purchased_item_id
-    validates :user_id
-    validates :item_id
-  end
-
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-
+class PurchasedItem < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :destinations
-
-  def save
-    purchased_item = PurchasedItem.create(user_id: user_id, item_id: item_id)
-
-    Destination.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, building_address: building_address, building_name: building_name, phone_number: phone_number, purchased_item_id: purchased_item_id)
-  end
 end
