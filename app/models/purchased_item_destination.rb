0f4f95fa6065b8fc79b476bedf0fa0b9b6da4ae1
imomochiii <orange.piano88@gmail.com>
2022-05-13 20:03:14 +0900

    Revert "Revert "エラーメッセージを設定""
    This reverts commit 85456e80a71b90c146717800b954120848b77c44.

--- app/models/purchased_item_destination.rb
+++ app/models/purchased_item_destination.rb
@@ -1,18 +1,19 @@
 class PurchasedItemDestination
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :building_address, :building_name, :phone_number, :purchased_item_id, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :building_address, :building_name, :phone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :city
+  end
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  with_options presence: true do
     validates :building_address
-    validates :phone_number, format: {with: /\A\d{10,11}\z/ }
-    validates :purchased_item_id
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is too short" }
     validates :user_id
     validates :item_id
   end
-
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number" }
 
   def save
     purchased_item = PurchasedItem.create(user_id: user_id, item_id: item_id)
